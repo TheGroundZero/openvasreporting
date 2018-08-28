938475b0836e64c1d00610e10654c5d80b2b0491
TheGroundZero <2406013+TheGroundZero@users.noreply.github.com>
2018-08-28 11:03:59 +0200

    Prep for 1.0.1b

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name='OpenVAS Reporting',
-    version='1.0.1a',
-    description='Convert OpenVAS XML report files to Excel reports.',
-    long_description='file: README.md',
-    author='TheGroundZero',
+    description='A tool to convert OpenVAS XML into reports.',
+    version='1.0.1b',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='TheGroundZero (@DezeStijn)',
     author_email='2406013+TheGroundZero@users.noreply.github.com',
     url='https://github.com/TheGroundZero/openvas_to_report',
     packages=find_packages(exclude=['tests', 'tests.*', 'venv', '*git*']),
@@ -20,8 +23,8 @@ setup(
     },
     project_urls={
         'Source Code': 'https://github.com/TheGroundZero/openvas_to_report',
-        'Documentation': 'https://github.com/TheGroundZero/openvas_to_report/blob/master/README.md',
-        'Issues': 'https://github.com/TheGroundZero/openvas_to_report/issues/'
+        'Documentation': 'https://openvas-reporting.stijncrevits.be',
+        'Issues': 'https://github.com/TheGroundZero/openvas_to_report/issues/',
     },
     license='GPL-3.0-or-later',
     keywords='OpenVAS OpenVAS-reports Excel xlsxwriter xlsx reporting reports report',
